b27e7e3c0b39e6cd4047e9a94badec90f69b9f46
Anas <anasroyal385@gmail.com>
2026-01-23 18:22:15 +0530

    add orderCancel and orderGet controller or routes

--- order/src/controller/order.controller.js
+++ order/src/controller/order.controller.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import orderModel from "../model/order.model.js";
 import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
 
 export const createOrder = async (req, res) => {
   const token = req.headers.authorization?.split(" ")[1];
@@ -63,4 +64,57 @@ export const createOrder = async (req, res) => {
   });
 
   res.status(201).json({ success: true, data: order });
+};
+
+export const getOrdersByUser = async (req, res) => {
+  try {
+    const orders = await orderModel.find({ userId: req.user.id });
+
+    if (orders.length === 0) {
+      return res
+        .status(404)
+        .json({ success: false, message: "No orders found" });
+    }
+
+    res.status(200).json({ success: true, data: orders });
+  } catch (err) {
+    res.status(500).json({ success: false, message: err.message });
+  }
+};
+
+export const cancelOrder = async (req, res) => {
+  try {
+    if (!req.params.id) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Order id is required" });
+    }
+
+    if (!mongoose.isValidObjectId(req.params.id)) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid order id" });
+    }
+
+    const order = await orderModel.findById(req.params.id);
+
+    if (!order) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Order not found" });
+    }
+
+    if (order.status !== "pending") {
+      return res
+        .status(400)
+        .json({ success: false, message: "Order cannot be canceled" });
+    }
+
+    order.status = "canceled";
+    await order.save();
+
+    res.status(200).json({ success: true, data: order });
+  } catch (err) {
+    res.status(500).json({ success: false, message: err.message });
+  }
 };
--- order/src/routes/order.routes.js
+++ order/src/routes/order.routes.js
@@ -3,7 +3,15 @@ import { Router } from "express";
 const router = Router();
 
 import * as orderController from "../controller/order.controller.js";
+import authenticate from "../middlewares/authenticate.middleware.js";
+import { roleAuth } from "../middlewares/roleAuth.middleware.js";
 
-router.post("/", orderController.createOrder);
+router.post("/", authenticate, roleAuth("user"), orderController.createOrder);
+router.get(
+  "/",
+  authenticate,
+  roleAuth("user"),
+  orderController.getOrdersByUser,
+);
 
 export default router;
